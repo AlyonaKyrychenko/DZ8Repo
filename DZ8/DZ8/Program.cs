2c385b86180ecedad15be69207a7020e21fe6333
AlyonaKyrychenko <i.alenka.ua@gmail.com>
2020-11-23 15:10:54 +0200

    Added reading from concole and writing on it

--- DZ8/DZ8/Program.cs
+++ DZ8/DZ8/Program.cs
@@ -10,7 +10,18 @@ namespace DZ8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            double a, b, c;
+
+            Console.WriteLine("Введите a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите c: ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Аргумент а равен {a}");
+            Console.WriteLine($"Аргумент b равен {b}");
+            Console.WriteLine($"Аргумент c равен {c}");
         }
     }
 }
