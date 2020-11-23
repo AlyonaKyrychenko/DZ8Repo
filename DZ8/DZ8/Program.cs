51e19ab4ae005e6ed5b4afb7b2af88432fd18af5
AlyonaKyrychenko <i.alenka.ua@gmail.com>
2020-11-23 16:37:31 +0200

    Added library Menu

--- DZ8/DZ8/Program.cs
+++ DZ8/DZ8/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CalculationLibrary;
 
 namespace DZ8
 {
@@ -10,8 +11,8 @@ namespace DZ8
     {
         static void Main(string[] args)
         {
-            double a, b, c, d, x, x1, x2;
 
+            double a, b, c, d, x, x1, x2;
 
             Console.WriteLine("Введите a: ");
             a = Convert.ToDouble(Console.ReadLine());
@@ -19,18 +20,20 @@ namespace DZ8
             b = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Введите c: ");
             c = Convert.ToDouble(Console.ReadLine());
-            
+
             d = b * b - 4 * a * c;
 
 
             if (d < 0)
             {
                 Console.WriteLine("Дискриминант меньше нуля. Корней нет.");
+                Console.WriteLine("\nНажмите enter чтобы продолжить.");
             }
             if (d == 0)
             {
                 x = -b / 2 * a;
                 Console.WriteLine($"Дискриминант равен нулю. Корень равен {x}");
+                Console.WriteLine("\nНажмите enter чтобы продолжить.");
             }
             if (d > 0)
             {
@@ -38,7 +41,9 @@ namespace DZ8
                 x1 = (-b - Math.Sqrt(d)) / 2 * a;
                 x2 = (-b + Math.Sqrt(d)) / 2 * a;
                 Console.WriteLine($"Дискриминант равен {d}. Первый корень равен {x1}. Второй корень равен {x2}.");
+                Console.WriteLine("\nНажмите enter чтобы продолжить.");
             }
+            Console.ReadLine();
         }
     }
 }
