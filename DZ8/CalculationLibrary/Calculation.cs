116472aaaf0d4346782fb69510685d8e391f7e4f
AlyonaKyrychenko <i.alenka.ua@gmail.com>
2020-11-23 17:17:49 +0200

    Added Calculation class

---
+++ DZ8/CalculationLibrary/Calculation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CalculationLibrary
+{
+    public class Calculation
+    {
+        private double A, B, C, d, x, x1, x2;
+        public Calculation(double a, double b, double c)
+        {
+            this.A = a;
+            this.B = b;
+            this.C = c;
+        }
+
+        public void Calculate ()
+        {
+            d = this.B * this.B - 4 * this.A * this.C;
+
+
+            if (d < 0)
+            {
+                Console.WriteLine("Дискриминант меньше нуля. Корней нет.");
+                Console.WriteLine("\nНажмите enter чтобы продолжить.");
+            }
+            if (d == 0)
+            {
+                x = -this.B / 2 * this.A;
+                Console.WriteLine($"Дискриминант равен нулю. Корень равен {x}");
+                Console.WriteLine("\nНажмите enter чтобы продолжить.");
+            }
+            if (d > 0)
+            {
+
+                x1 = (-this.B - Math.Sqrt(d)) / 2 * this.A;
+                x2 = (-this.B + Math.Sqrt(d)) / 2 * this.A;
+                Console.WriteLine($"Дискриминант равен {d}. Первый корень равен {x1}. Второй корень равен {x2}.");
+                Console.WriteLine("\nНажмите enter чтобы продолжить.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- DZ8/DZ8/Program.cs
+++ DZ8/DZ8/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CalculationLibrary;
+﻿using CalculationLibrary;
 
 namespace DZ8
 {
@@ -23,34 +18,12 @@ namespace DZ8
                 switch (userChoice)
                 {
                     case 1:
-                        double d, x, x1, x2;
+
                         double a = menu.GetDouble("Введите a: ");
                         double b = menu.GetDouble("Введите b: ");
                         double c = menu.GetDouble("Введите c: ");
-
-                        d = b * b - 4 * a * c;
-
-
-                        if (d < 0)
-                        {
-                            Console.WriteLine("Дискриминант меньше нуля. Корней нет.");
-                            Console.WriteLine("\nНажмите enter чтобы продолжить.");
-                        }
-                        if (d == 0)
-                        {
-                            x = -b / 2 * a;
-                            Console.WriteLine($"Дискриминант равен нулю. Корень равен {x}");
-                            Console.WriteLine("\nНажмите enter чтобы продолжить.");
-                        }
-                        if (d > 0)
-                        {
-
-                            x1 = (-b - Math.Sqrt(d)) / 2 * a;
-                            x2 = (-b + Math.Sqrt(d)) / 2 * a;
-                            Console.WriteLine($"Дискриминант равен {d}. Первый корень равен {x1}. Второй корень равен {x2}.");
-                            Console.WriteLine("\nНажмите enter чтобы продолжить.");
-                        }
-                        Console.ReadLine();
+                        Calculation calculation = new Calculation(a, b, c);
+                        calculation.Calculate();
                         break;
 
                     case 0:
