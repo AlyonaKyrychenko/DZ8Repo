2ee7a67700c073bee96c73760ebb64f124948e26
AlyonaKyrychenko <i.alenka.ua@gmail.com>
2020-11-23 16:44:48 +0200

    Added Menu and validation

--- DZ8/CalculationLibrary/Menu.cs
+++ DZ8/CalculationLibrary/Menu.cs
@@ -8,7 +8,58 @@ namespace CalculationLibrary
 {
     public class Menu
     {
+        public double GetDouble(string prompt)
+        {
+            string input;
+            double result;
+            Console.Clear();
+            Console.Write(prompt);
+            while (true)
+            {
+                input = Console.ReadLine();
+                try
+                {
+                    result = Double.Parse(input);
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Не удалось распознать введенное число. Повторите ввод.");
+                }
+            }
+            return result;
+        }
+        public int DoMenu(int variant)
+        {
+            int result;
+            Console.Clear();
+            Console.WriteLine("Выбирите действие:");
+            Console.WriteLine("1 - новый рассчет.");
+            Console.WriteLine("0 - выйти из программы.");
 
-    
+            while (true)
+            {
+                string input = Console.ReadLine();
+                try
+                {
+                    int minMenuValue = 0;
+                    int maxMenuValue = 2;
+                    result = Int32.Parse(input);
+                    if (result >= minMenuValue && result <= maxMenuValue)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Не удалось распознать введенное число. Повторите ввод.");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Не удалось распознать введенное число. Повторите ввод.");
+                }
+            }
+            return result;
+        }
     }
 }
--- DZ8/DZ8/Program.cs
+++ DZ8/DZ8/Program.cs
@@ -12,38 +12,52 @@ namespace DZ8
         static void Main(string[] args)
         {
 
-            double a, b, c, d, x, x1, x2;
+            Menu menu = new Menu();
+            int userChoice;
+            bool exitApp = false;
 
-            Console.WriteLine("Введите a: ");
-            a = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введите b: ");
-            b = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введите c: ");
-            c = Convert.ToDouble(Console.ReadLine());
-
-            d = b * b - 4 * a * c;
-
-
-            if (d < 0)
+            while (!exitApp)
             {
-                Console.WriteLine("Дискриминант меньше нуля. Корней нет.");
-                Console.WriteLine("\nНажмите enter чтобы продолжить.");
-            }
-            if (d == 0)
-            {
-                x = -b / 2 * a;
-                Console.WriteLine($"Дискриминант равен нулю. Корень равен {x}");
-                Console.WriteLine("\nНажмите enter чтобы продолжить.");
-            }
-            if (d > 0)
-            {
+                userChoice = menu.DoMenu(0);
 
-                x1 = (-b - Math.Sqrt(d)) / 2 * a;
-                x2 = (-b + Math.Sqrt(d)) / 2 * a;
-                Console.WriteLine($"Дискриминант равен {d}. Первый корень равен {x1}. Второй корень равен {x2}.");
-                Console.WriteLine("\nНажмите enter чтобы продолжить.");
+                switch (userChoice)
+                {
+                    case 1:
+                        double d, x, x1, x2;
+                        double a = menu.GetDouble("Введите a: ");
+                        double b = menu.GetDouble("Введите b: ");
+                        double c = menu.GetDouble("Введите c: ");
+
+                        d = b * b - 4 * a * c;
+
+
+                        if (d < 0)
+                        {
+                            Console.WriteLine("Дискриминант меньше нуля. Корней нет.");
+                            Console.WriteLine("\nНажмите enter чтобы продолжить.");
+                        }
+                        if (d == 0)
+                        {
+                            x = -b / 2 * a;
+                            Console.WriteLine($"Дискриминант равен нулю. Корень равен {x}");
+                            Console.WriteLine("\nНажмите enter чтобы продолжить.");
+                        }
+                        if (d > 0)
+                        {
+
+                            x1 = (-b - Math.Sqrt(d)) / 2 * a;
+                            x2 = (-b + Math.Sqrt(d)) / 2 * a;
+                            Console.WriteLine($"Дискриминант равен {d}. Первый корень равен {x1}. Второй корень равен {x2}.");
+                            Console.WriteLine("\nНажмите enter чтобы продолжить.");
+                        }
+                        Console.ReadLine();
+                        break;
+
+                    case 0:
+                        exitApp = true;
+                        break;
+                }
             }
-            Console.ReadLine();
         }
     }
 }
